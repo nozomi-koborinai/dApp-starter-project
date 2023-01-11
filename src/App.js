96042056eff2f9f2b9de55dbfef9070d6737f9a0
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-01-12 04:14:40 +0900

    🚀 feat: WavePortalコントラクト側から発火されるwaveイベントをフロントエンド…

--- src/App.js
+++ src/App.js
@@ -19,6 +19,87 @@ const App = () => {
    */
   const contractABI = abi.abi;
 
+  /* すべてのwavesを保存する状態変数を定義 */
+  const [allWaves, setAllWaves] = useState([]);
+
+  const getAllWaves = async () => {
+    const { ethereum } = window;
+
+    try {
+      if (ethereum) {
+        // プロバイダー（MetaMask）を取得
+        // これにより、フロントエンドがMetaMaskを介して、イーサリアムノードに接続できる
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        /* コントラクトからgetAllWavesメソッドを呼び出す */
+        const waves = await wavePortalContract.getAllWaves();
+        /* UIに必要なのは、アドレス、タイムスタンプ、メッセージだけなので、以下のように設定 */
+        const wavesCleaned = waves.map((wave) => {
+          return {
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message,
+          };
+        });
+
+        /* React Stateにデータを格納する */
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * `emit`されたイベントに反応する
+   */
+  useEffect(() => {
+    let wavePortalContract;
+
+    // 「waveを送ったイベント」に対するイベントリスナ（イベントハンドラ）
+    const onNewWave = (from, timestamp, message) => {
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves((prevState) => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message: message,
+        },
+      ]);
+    };
+
+    /* NewWaveイベントがコントラクトから発信されたときに、情報を受け取ります */
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      wavePortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      // WavePortalコントラクトのNewWaveイベントが発火したらフロント側のonNewWaveをコール
+      // .NETでいうところのAddEventHandler
+      wavePortalContract.on("NewWave", onNewWave);
+    }
+    /*メモリリークを防ぐために、NewWaveのイベントを解除します*/
+    return () => {
+      if (wavePortalContract) {
+        // .NETでいうところのRemoveEventHandler
+        wavePortalContract.off("NewWave", onNewWave);
+      }
+    };
+  }, []);
+
   /*
    * window.ethereumにアクセスできることを確認します。
    */
